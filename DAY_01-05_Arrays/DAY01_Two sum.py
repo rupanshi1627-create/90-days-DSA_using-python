e0a087aa84909bb80ec766d2fc2e3fa102f84203
Rupanshi <rupanshi1627@gmail.com>
2026-04-11 21:06:29 +0530

    two sum

--- DAY_01-05_Arrays/DAY01_Two sum.py
+++ DAY_01-05_Arrays/DAY01_Two sum.py
@@ -19,3 +19,28 @@ target = 9
 # Call function and print result
 print(obj.twoSum(nums, target))
 print(nums[0], nums[1])  # This will print the values at indices 0 and 1, which are 2 and 7 respectively.
+
+
+#METHOD 2: Using Hash Map (Optimized Approach)
+class Solution:
+    def twoSum(self, nums, target):
+        d = {}  # number → index
+
+        for i in range(len(nums)):
+            y = nums[i]
+            x = target - y  # x + y = target
+
+            if x in d:
+                return [d[x], i]
+
+            d[y] = i
+
+
+# --- run in VS Code ---
+
+obj = Solution()
+
+nums = [2, 7, 11, 15]
+target = 9
+
+print(obj.twoSum(nums, target))
